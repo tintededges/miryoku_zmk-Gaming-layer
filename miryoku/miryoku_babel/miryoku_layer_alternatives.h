0b760873944037d1fc65349915461671ecc8180c
tintededges <123136929+tintededges@users.noreply.github.com>
2024-09-14 22:23:35 +0200

    Move toggle to be in same place for both layers

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -388,13 +388,13 @@ U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BT
 
 
 #define MIRYOKU_ALTERNATIVES_GAME_FLIP \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp TAB,           &kp N4,            &kp N3,            &kp N2,            &kp N1,            \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &tog U_GAME,       &kp D,             &kp W,             &kp A,             &kp LSHFT,         \
-U_NA,              &kp RALT,          &u_to_U_MOUSE,     &u_to_U_GAME,      U_NA,              &kp E,             &kp F,             &kp S,             &kp Q,             &kp LCTRL,         \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &tog U_GAME,       &kp N4,            &kp N3,            &kp N2,            &kp N1,            \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp F,             &kp D,             &kp W,             &kp A,             &kp LSHFT,         \
+U_NA,              &kp RALT,          &u_to_U_MOUSE,     &u_to_U_GAME,      U_NA,              &kp TAB,           &kp e,             &kp S,             &kp Q,             &kp LCTRL,         \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp SPACE,         &kp LCTRL,         &kp ESC,           U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_GAME \
-&kp N1,            &kp N2,            &kp N3,            &kp N4,            &kp TAB,           U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
-&kp LSHFT,         &kp A,             &kp W,             &kp D,             &tog U_GAME,       U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-&kp LCTRL,         &kp Q,             &kp S,             &kp F,             &kp E,             U_NA,              &u_to_U_GAME,      &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
+&kp N1,            &kp N2,            &kp N3,            &kp N4,            &tog U_GAME,       U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp LSHFT,         &kp A,             &kp W,             &kp D,             &kp F,             U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp LCTRL,         &kp Q,             &kp S,             &kp E,             &kp TAB,           U_NA,              &u_to_U_GAME,      &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp ESC,           &kp LCTRL,         &kp SPACE,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
