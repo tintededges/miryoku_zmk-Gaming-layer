10be078eff1c153e7aa2295183e314ef68e30a05
tintededges <123136929+tintededges@users.noreply.github.com>
2024-09-15 03:34:53 +0200

    Add files via upload

--- miryoku/miryoku_babel/miryoku_layer_list.h
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -11,8 +11,8 @@ MIRYOKU_X(BASE,   "Base") \
 MIRYOKU_X(EXTRA,  "Extra") \
 MIRYOKU_X(TAP,    "Tap") \
 MIRYOKU_X(BUTTON, "Button") \
-MIRYOKU_X(NAV,    "Nav") \
 MIRYOKU_X(MOUSE,  "Mouse") \
+MIRYOKU_X(NAV,    "Nav") \
 MIRYOKU_X(MEDIA,  "Media") \
 MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
@@ -23,8 +23,8 @@ MIRYOKU_X(GAME,   "Game")
 #define U_EXTRA  1
 #define U_TAP    2
 #define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
+#define U_MOUSE  4
+#define U_NAV    5
 #define U_MEDIA  6
 #define U_NUM    7
 #define U_SYM    8
